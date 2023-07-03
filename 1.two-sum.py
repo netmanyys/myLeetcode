2ef834c69034944dd6c745846e1c8ac0b00ae2bf
Steven Yan <yunsheng.yan@walmart.com>
2023-07-03 11:34:12 -0700

    rewrite leetcode 1 two sum with hashmap and time complex is O(n), space complex is also O(n)

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -3,21 +3,55 @@
 #
 # [1] Two Sum
 #
+# 1. Two Sum
+# Easy
+# 48.1K
+# 1.6K
+# Companies
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
 
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mydict = {}
-        for index, num in enumerate(nums):
-            mydict[num] = index 
-        for index, num in enumerate(nums):
-            if (target - num) in nums:
-                if mydict[(target - num)] == index:
-                    continue
-                else:
-                    if index <= mydict[(target-num)]:
-                        return [index, mydict[(target-num)]]
-                    else:
-                        return [mydict[(target-num)], index]
+        prevMap  = {}
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in  prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
+
 # @lc code=end
 
